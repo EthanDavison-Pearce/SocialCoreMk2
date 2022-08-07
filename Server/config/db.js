bca12b80a303ad2a5aad08bb215e6195b56d8a1c
Ethan D-P <ethan@reth.com>
2022-08-07 17:30:11 +0100

    MongoDbChange

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,14 +1,3 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-const username = "Randomedp";
-const password = "uPU7igXXduYngN5u";
-const cluster = "Cluster0";
-const dbname = "SocialCore";
-module.exports = () => mongoose.connect(
-    `mongodb+srv://Randomedp:uPU7igXXduYngN5u@cluster0.nurp4vb.mongodb.net/?retryWrites=true&w=majority`, 
-    {
-      useNewUrlParser: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true
-    }
-  );
+module.exports = () => mongoose.connect('mongodb://Randomedp:uPU7igXXduYngN5u@cluster0.nurp4vb.mongodb.net/?retryWrites=true&w=majority');
